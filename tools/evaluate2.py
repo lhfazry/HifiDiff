1b3caf2f637430f3f0195051c2c979cfb4cb95d9
Fazry <lhuqita.fazry@gmail.com>
2023-02-08 17:21:48 +0700

    add new evaluate

--- tools/evaluate2.py
+++ tools/evaluate2.py
@@ -3,21 +3,13 @@ import os
 import subprocess
 import sys
 import glob
-import librosa
 from tools.preprocess import get_mel
 import numpy as np
 import torch
-from nnmnkwii.metrics import melcd
 from argparse import ArgumentParser
 from tools.preprocess import MAX_WAV_VALUE, get_mel, normalize
-from params import params
-from scipy.io.wavfile import read
-import pyworld
-from scipy.io import wavfile
-import pysptk
-from scipy.spatial.distance import euclidean
 import os
-from fastdtw import fastdtw
+from pathlib import Path
 from mel_cepstral_distance import get_metrics_wavs, get_metrics_mels
 
 
@@ -27,7 +19,8 @@ def main(args):
     sr = 22050
 
     for fname in os.listdir(args.sdir):
-        mcd, penalty = get_metrics_wavs(os.path.join(args.sdir, fname), os.path.join(args.odir, fname))
+        mcd, penalty = get_metrics_wavs(Path(os.path.join(args.sdir, fname)), 
+            Path(os.path.join(args.odir, fname)))
 
         results += mcd
         total += 1
