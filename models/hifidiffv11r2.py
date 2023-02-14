10b56b407b6337d6c698d4a0adab50310059b111
Fazry <lhuqita.fazry@gmail.com>
2023-02-14 16:51:31 +0700

    fixing bugs

--- models/hifidiffv11r2.py
+++ models/hifidiffv11r2.py
@@ -126,15 +126,15 @@ class ResidualBlock(nn.Module):
         if index % 2 == 0:
             y = self.dilated_conv(y) + conditioner
 
-            print(f"y: {y.shape}")
-            print(f"conditioner: {conditioner.shape}")
+            #print(f"y: {y.shape}")
+            #print(f"conditioner: {conditioner.shape}")
 
             if conditioner_global is not None:
                 y = y + self.conditioner_projection_global(conditioner_global)
 
             if f0 is not None:
                 f0 = self.f0_projection(f0)
-                print(f"f0: {f0.shape}")
+                #print(f"f0: {f0.shape}")
 
                 y = y + f0
         else:
@@ -204,9 +204,9 @@ class HifiDiffV11R2(nn.Module):
         x = self.input_projection(x)
         x = F.relu(x)
 
-        print(f"Audio: {audio.shape}")
-        print(f"spectrogram: {spectrogram.shape}")
-        print(f"f0: {f0.shape}")
+        #print(f"Audio: {audio.shape}")
+        #print(f"spectrogram: {spectrogram.shape}")
+        #print(f"f0: {f0.shape}")
 
         diffusion_step = self.diffusion_embedding(diffusion_step)
         spectrogram = self.spectrogram_upsampler(spectrogram)
