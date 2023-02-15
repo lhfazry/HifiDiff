f71ed1408de9c8e8d594c0e2456182a4c5220284
Fazry <lhuqita.fazry@gmail.com>
2023-02-15 16:28:37 +0700

    add new tools

---
+++ tools/resample.py
@@ -0,0 +1,25 @@
+import os
+from argparse import ArgumentParser
+from glob import glob
+from pathlib import Path
+import librosa
+import soundfile as sf
+
+
+def main(args):
+    if not os.path.exists(args.odir):
+        os.makedirs(args.odir)
+
+    for fname in glob(os.path.join(args.idir, "*.wav")):
+        wav, sr = librosa.load(fname, sr=args.sr)
+        #resample_wav = resample(wav, args.sr)
+        #write(os.path.join(args.odir, Path(fname).name), args.sr, wav)
+        sf.write(os.path.join(args.odir, Path(fname).name), wav, sr, format='wav')
+    
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Resample wav')
+    parser.add_argument('--idir', help='Input directory of waveform')
+    parser.add_argument('--odir', help='Output directory of waveform')
+    parser.add_argument('--sr', type=int, default=22050, help='New sr')
+    
+    main(parser.parse_args())
