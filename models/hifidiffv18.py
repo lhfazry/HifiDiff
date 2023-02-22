400c86365fa6122413dd4a434b3a64c4a4e3810b
Fazry <lhuqita.fazry@gmail.com>
2023-02-23 02:38:21 +0700

    add new config

--- models/hifidiffv18.py
+++ models/hifidiffv18.py
@@ -265,15 +265,14 @@ class HifiDiffV18(nn.Module):
         hf_x = hf_x.unsqueeze(1) # b 1 c t
         lf_x = lf_x.unsqueeze(1) # b 1 c t
         x = torch.cat([hf_x, lf_x], dim=1) # b 2 c t
-        print(x.shape)
 
         x = rearrange(x, "b d c t -> (b d) c t")
         x = self.skip_projection(x)
         x = F.relu(x)
-        x = self.output_projection(x)
-        print(x.shape)
+        x = self.output_projection(x) # (b d) 1 t
 
         x = rearrange(x, "(b d) c t -> b d c t", d=2)
+        print(x.shape)
         hf_x, lf_x = torch.chunk(x, 2, dim=1) # hf_x => b 1 c t, lf_x => b 1 c t, 
         hf_x = hf_x.squeeze() # b c t
         lf_x = lf_x.squeeze() # b c t
