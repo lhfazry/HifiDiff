620f292bddbddd0fd4a70f75aa2d735282dd52a9
Fazry <lhuqita.fazry@gmail.com>
2023-02-25 21:09:04 +0700

    add more scripts

---
+++ scripts/kagayaki/infer_hifidiffv18r4_mstft.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -q GPU-1
+#PBS -N infer_hifidiffv18r4_mstft
+#PBS -l select=1:ngpus=1
+#PBS -j oe
+#PBS -M s2212015@jaist.ac.jp -m be
+
+module load singularity
+cd ~/HifiDiff
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
+python inference.py checkpoints/hifidiffv18r4_mstft \
+    /home/s2212015/LJSpeech-1.1 \
+    filelists/test.txt \
+    --step 500000 \
+    --fast_iter 50 >> logs/inferences/hifidiffv18r4_mstft.log 2>&1 && 
+    python inference.py checkpoints/hifidiffv18r4_mstft \
+    /home/s2212015/indsp \
+    filelists/test_bali.txt \
+    --step 500000 \
+    --fast_iter 50 >> logs/inferences/hifidiffv18r4_mstft.log 2>&1
+    
