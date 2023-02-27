7dd9cecc620466c170a8e1e9d921e9ebb78a3e97
Fazry <lhuqita.fazry@gmail.com>
2023-02-27 15:53:10 +0700

    add new config

--- models/hifidiffv18r8.py
+++ models/hifidiffv18r8.py
@@ -132,13 +132,13 @@ class ResidualBlock(nn.Module):
 class LFResidualBlock(nn.Module):
     def __init__(self, n_mels, residual_channels, dilation, n_cond_global=None):
         super().__init__()
-        self.dilated_conv = Conv1d(residual_channels, 2 * residual_channels, 3, padding=dilation, dilation=dilation)
+        self.dilated_conv = Conv1d(residual_channels, residual_channels, 3, padding=dilation, dilation=dilation)
         self.diffusion_projection = Linear(512, residual_channels)
-        self.conditioner_projection = Conv1d(n_mels, 2 * residual_channels, 1)
+        self.conditioner_projection = Conv1d(n_mels, residual_channels, 1)
         if n_cond_global is not None:
-            self.conditioner_projection_global = Conv1d(n_cond_global, 2 * residual_channels, 1)
+            self.conditioner_projection_global = Conv1d(n_cond_global, residual_channels, 1)
         self.output_projection = Conv1d(residual_channels, 2 * residual_channels, 1)
-        self.snake = Snake(2 * residual_channels)
+        self.snake = Snake(residual_channels)
 
     def forward(self, x, conditioner, diffusion_step, conditioner_global=None):
         # x ==> (b d), c, t
@@ -151,8 +151,8 @@ class LFResidualBlock(nn.Module):
         if conditioner_global is not None:
             y = y + self.conditioner_projection_global(conditioner_global)
 
-        gate, filter = torch.chunk(y, 2, dim=1)
-        y = torch.sigmoid(gate) * torch.tanh(filter)
+        #gate, filter = torch.chunk(y, 2, dim=1)
+        y = torch.tanh(y)
         #y = self.snake(y)
 
         y = self.output_projection(y)
