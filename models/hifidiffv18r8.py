daba41617bceeafaebddeeb687e5709b1d31985f
Fazry <lhuqita.fazry@gmail.com>
2023-02-27 09:05:35 +0700

    add new config

--- models/hifidiffv18r8.py
+++ models/hifidiffv18r8.py
@@ -132,7 +132,7 @@ class ResidualBlock(nn.Module):
 class LFResidualBlock(nn.Module):
     def __init__(self, n_mels, residual_channels, dilation, n_cond_global=None):
         super().__init__()
-        self.dilated_conv = Conv1d(residual_channels, 2 * residual_channels, 3, padding=dilation, dilation=dilation)
+        self.dilated_conv = Conv1d(residual_channels, residual_channels, 3, padding=dilation, dilation=dilation)
         self.diffusion_projection = Linear(512, residual_channels)
         self.conditioner_projection = Conv1d(n_mels, residual_channels, 1)
         if n_cond_global is not None:
