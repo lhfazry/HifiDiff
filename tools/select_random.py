4292d6869a1f8ee2b3decfe180fb2c2f3ff97d16
Fazry <lhuqita.fazry@gmail.com>
2023-03-04 15:52:32 +0700

    add new script

--- tools/select_random.py
+++ tools/select_random.py
@@ -8,11 +8,13 @@ def select_random(args):
     files = glob(os.path.join(args.input_dir,  "*.wav"))
     selecteds = np.random.choice(files, 100, False)
     residuals = [item for item in files if item not in selecteds]
+    
     print(f"files size: {len(files)}")
     print(f"selecteds size: {len(selecteds)}")
     print(f"residuals size: {len(residuals)}")
-    #for f in residuals:
-    #    os.remove(f)
+    
+    for f in residuals:
+        os.remove(f)
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Select random')
