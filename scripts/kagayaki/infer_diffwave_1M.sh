035e366d67921ab6dcccc3341f2c716de5d210fc
Fazry <lhuqita.fazry@gmail.com>
2023-03-05 03:41:00 +0700

    add new script

--- scripts/kagayaki/infer_diffwave_1M.sh
+++ scripts/kagayaki/infer_diffwave_1M.sh
@@ -11,31 +11,7 @@ cd ~/HifiDiff
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
     checkpoints/diffwave \
     /home/s2212015/LJSpeech-1.1 \
-    filelists/test.txt \
-    --step 1000000 \
-    --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
-singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
-    checkpoints/diffwave \
-    /home/s2212015/indsp \
-    filelists/test_bali.txt \
-    --step 1000000 \
-    --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
-singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
-    checkpoints/diffwave \
-    /home/s2212015/indsp \
-    filelists/test_batak.txt \
-    --step 1000000 \
-    --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
-singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
-    checkpoints/diffwave \
-    /home/s2212015/indsp \
-    filelists/test_jawa.txt \
-    --step 1000000 \
-    --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
-singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
-    checkpoints/diffwave \
-    /home/s2212015/indsp \
-    filelists/test_sunda.txt \
+    filelists/all_test_kagayaki.txt \
     --step 1000000 \
     --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
     
