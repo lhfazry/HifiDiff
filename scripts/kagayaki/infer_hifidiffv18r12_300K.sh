61ac3a63513af64d6fd6e68c58d8071e0727a8b8
Fazry <lhuqita.fazry@gmail.com>
2023-03-04 16:25:58 +0700

    add new script

--- scripts/kagayaki/infer_hifidiffv18r12_300K.sh
+++ scripts/kagayaki/infer_hifidiffv18r12_300K.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -q GPU-1A
-#PBS -N infer_diffwave
+#PBS -N infer_hifidiffv18r12
 #PBS -l select=1:ngpus=1
 #PBS -j oe
 #PBS -M s2212015@jaist.ac.jp -m be
@@ -9,8 +9,8 @@
 module load singularity
 cd ~/HifiDiff
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
-    checkpoints/diffwave \
+    checkpoints/hifidiffv18r12 \
     /home/s2212015/LJSpeech-1.1 \
     filelists/all_test_kagayaki.txt \
     --step 300000 \
-    --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
+    --fast_iter 50 >> logs/inferences/hifidiffv18r12.log 2>&1
