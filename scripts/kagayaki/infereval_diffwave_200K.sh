b5272c92496527e83c5db57cdaa0a5e37f0c9836
Fazry <lhuqita.fazry@gmail.com>
2023-03-06 06:54:03 +0700

    add new script

---
+++ scripts/kagayaki/infereval_diffwave_200K.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#PBS -q GPU-1
+#PBS -N infer_diffwave
+#PBS -l select=1:ngpus=1
+#PBS -j oe
+#PBS -M s2212015@jaist.ac.jp -m be
+
+module load singularity
+cd ~/HifiDiff
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python inference.py \
+    checkpoints/diffwave \
+    /home/s2212015/LJSpeech-1.1 \
+    filelists/all_test_kagayaki.txt \
+    --step 200000 \
+    --fast_iter 50 >> logs/inferences/diffwave.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix LJ \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/LJSpeech-1.1/wavs >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind001_F_Bli \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind001_F_Bli/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind002_M_Bli \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind002_M_Bli/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind001_F_Btk \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind001_F_Btk/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind002_M_Btk \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind002_M_Btk/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind001_F_Jaw \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind001_F_Jaw/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind002_M_Jaw \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind002_M_Jaw/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind001_F_Snd \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind001_F_Snd/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix Ind002_M_Snd \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/indsp/Ind002_M_Snd/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix afr_ \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/datasets/af_za/za/afr/wavs/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix arf_ \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/datasets/es_ar_female/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix sso_ \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/datasets/st_za/za/sso/wavs/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix tsn_ \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/datasets/tn_za/za/tsn/wavs/ >> logs/evaluation/diffwave_step200000.log 2>&1
+
+singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
+    --prefix xho_ \
+    --sdir checkpoints/sample_slow/diffwave_step200000 \
+    --odir /home/s2212015/datasets/xh_za/za/xho/wavs/ >> logs/evaluation/diffwave_step200000.log 2>&1
