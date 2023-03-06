8894bf01d7fcdb5b970677c06748e0a2f19fdb79
Fazry <lhuqita.fazry@gmail.com>
2023-03-07 04:13:50 +0700

    add more scripts

--- scripts/kagayaki/nisqa_diffwave_50K.sh
+++ scripts/kagayaki/nisqa_diffwave_50K.sh
@@ -12,103 +12,103 @@ cd ~/NISQA
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Bli \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind001_F_Bli >> logs/diffwave_step100000_Ind001_F_Bli.log
+--filename diffwave_step50000_Ind001_F_Bli >> logs/diffwave_step50000_Ind001_F_Bli.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind002_M_Bli \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind002_M_Bli >> logs/diffwave_step100000_Ind002_M_Bli.log
+--filename diffwave_step50000_Ind002_M_Bli >> logs/diffwave_step50000_Ind002_M_Bli.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Btk \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind001_F_Btk >> logs/diffwave_step100000_Ind001_F_Btk.log
+--filename diffwave_step50000_Ind001_F_Btk >> logs/diffwave_step50000_Ind001_F_Btk.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind002_M_Btk \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind002_M_Btk >> logs/diffwave_step100000_Ind002_M_Btk.log
+--filename diffwave_step50000_Ind002_M_Btk >> logs/diffwave_step50000_Ind002_M_Btk.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Jaw \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind001_F_Jaw >> logs/diffwave_step100000_Ind001_F_Jaw.log
+--filename diffwave_step50000_Ind001_F_Jaw >> logs/diffwave_step50000_Ind001_F_Jaw.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind002_M_Jaw \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind002_M_Jaw >> logs/diffwave_step100000_Ind002_M_Jaw.log
+--filename diffwave_step50000_Ind002_M_Jaw >> logs/diffwave_step50000_Ind002_M_Jaw.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Snd \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_Ind001_F_Snd >> logs/diffwave_step100000_Ind001_F_Snd.log
+--filename diffwave_step50000_Ind001_F_Snd >> logs/diffwave_step50000_Ind001_F_Snd.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix afr_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_afr >> logs/diffwave_step100000_afr.log
+--filename diffwave_step50000_afr >> logs/diffwave_step50000_afr.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix arf_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_arf >> logs/diffwave_step100000_arf.log
+--filename diffwave_step50000_arf >> logs/diffwave_step50000_arf.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix sso_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_sso >> logs/diffwave_step100000_sso.log
+--filename diffwave_step50000_sso >> logs/diffwave_step50000_sso.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix tsn_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_tsn >> logs/diffwave_step100000_tsn.log
+--filename diffwave_step50000_tsn >> logs/diffwave_step50000_tsn.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix xho_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_xho >> logs/diffwave_step100000_xho.log
+--filename diffwave_step50000_xho >> logs/diffwave_step50000_xho.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix xho_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step100000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/diffwave_step50000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename diffwave_step100000_xho >> logs/diffwave_step100000_xho.log
+--filename diffwave_step50000_xho >> logs/diffwave_step50000_xho.log
--- scripts/kagayaki/nisqa_hifidiffv18r18_100K.sh
+++ scripts/kagayaki/nisqa_hifidiffv18r18_100K.sh
@@ -12,103 +12,103 @@ cd ~/NISQA
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Bli \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind001_F_Bli >> logs/hifidiffv18r18_step50000_Ind001_F_Bli.log
+--filename hifidiffv18r18_step100000_Ind001_F_Bli >> logs/hifidiffv18r18_step100000_Ind001_F_Bli.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind002_M_Bli \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind002_M_Bli >> logs/hifidiffv18r18_step50000_Ind002_M_Bli.log
+--filename hifidiffv18r18_step100000_Ind002_M_Bli >> logs/hifidiffv18r18_step100000_Ind002_M_Bli.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Btk \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind001_F_Btk >> logs/hifidiffv18r18_step50000_Ind001_F_Btk.log
+--filename hifidiffv18r18_step100000_Ind001_F_Btk >> logs/hifidiffv18r18_step100000_Ind001_F_Btk.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind002_M_Btk \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind002_M_Btk >> logs/hifidiffv18r18_step50000_Ind002_M_Btk.log
+--filename hifidiffv18r18_step100000_Ind002_M_Btk >> logs/hifidiffv18r18_step100000_Ind002_M_Btk.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Jaw \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind001_F_Jaw >> logs/hifidiffv18r18_step50000_Ind001_F_Jaw.log
+--filename hifidiffv18r18_step100000_Ind001_F_Jaw >> logs/hifidiffv18r18_step100000_Ind001_F_Jaw.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind002_M_Jaw \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind002_M_Jaw >> logs/hifidiffv18r18_step50000_Ind002_M_Jaw.log
+--filename hifidiffv18r18_step100000_Ind002_M_Jaw >> logs/hifidiffv18r18_step100000_Ind002_M_Jaw.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix Ind001_F_Snd \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_Ind001_F_Snd >> logs/hifidiffv18r18_step50000_Ind001_F_Snd.log
+--filename hifidiffv18r18_step100000_Ind001_F_Snd >> logs/hifidiffv18r18_step100000_Ind001_F_Snd.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix afr_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_afr >> logs/hifidiffv18r18_step50000_afr.log
+--filename hifidiffv18r18_step100000_afr >> logs/hifidiffv18r18_step100000_afr.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix arf_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_arf >> logs/hifidiffv18r18_step50000_arf.log
+--filename hifidiffv18r18_step100000_arf >> logs/hifidiffv18r18_step100000_arf.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix sso_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_sso >> logs/hifidiffv18r18_step50000_sso.log
+--filename hifidiffv18r18_step100000_sso >> logs/hifidiffv18r18_step100000_sso.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix tsn_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_tsn >> logs/hifidiffv18r18_step50000_tsn.log
+--filename hifidiffv18r18_step100000_tsn >> logs/hifidiffv18r18_step100000_tsn.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix xho_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_xho >> logs/hifidiffv18r18_step50000_xho.log
+--filename hifidiffv18r18_step100000_xho >> logs/hifidiffv18r18_step100000_xho.log
 
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python run_predict.py \
 --mode predict_dir --prefix xho_ \
 --pretrained_model weights/nisqa_tts.tar \
---data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step50000/ \
+--data_dir ../HifiDiff/checkpoints/sample_slow/hifidiffv18r18_step100000/ \
 --num_workers 0 --bs 10 \
 --output_dir results \
---filename hifidiffv18r18_step50000_xho >> logs/hifidiffv18r18_step50000_xho.log
+--filename hifidiffv18r18_step100000_xho >> logs/hifidiffv18r18_step100000_xho.log
