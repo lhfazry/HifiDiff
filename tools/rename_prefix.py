ea38b21e4f85c045e8a518d1c454123d367bb4cb
Fazry <lhuqita.fazry@gmail.com>
2023-03-09 09:05:24 +0700

    add new tools

---
+++ tools/rename_prefix.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import numpy as np
+from glob import glob
+from argparse import ArgumentParser
+from pathlib import Path
+
+def rename_prefix(args):
+    files = glob(os.path.join(args.input_dir,  f"{args.old_prefix}*.wav"))
+    
+    for f in files:
+        pf = Path(f)
+        new_fname = pf.name.replce(args.old_prefix, args.new_prefix)
+        os.rename(f, os.path.join(pf.parent, new_fname))
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Select random')
+    parser.add_argument('--input_dir', help='folder')
+    parser.add_argument('--old_prefix')
+    parser.add_argument('--new_prefix')
+    args = parser.parse_args()
+
+    rename_prefix(args)
