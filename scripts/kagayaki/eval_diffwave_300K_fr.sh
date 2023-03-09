a61aba05d3a5356660c9222b260ec0519528e04d
Fazry <lhuqita.fazry@gmail.com>
2023-03-09 10:02:51 +0700

    add new script

--- scripts/kagayaki/eval_diffwave_300K_fr.sh
+++ scripts/kagayaki/eval_diffwave_300K_fr.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #PBS -q GPU-1
-#PBS -N eval_hifidiffv18r18
+#PBS -N eval_diffwave
 #PBS -l select=1:ngpus=1
 #PBS -j oe
 #PBS -M s2212015@jaist.ac.jp -m be
 
 module load singularity
 cd ~/HifiDiff
-
 singularity exec -i --nv ~/pytorch_22.02-py3.sif python tools/evaluate2.py \
     --prefix fr_ \
-    --sdir checkpoints/sample_slow/hifidiffv18r18_step300000 \
-    --odir /home/s2212015/datasets/fr/CLEM_HDD/IRCAM/Open_SLR/wav/ >> logs/evaluation/hifidiffv18r18.log 2>&1
+    --sdir checkpoints/sample_slow/diffwave_step300000 \
+    --odir /home/s2212015/datasets/fr/CLEM_HDD/IRCAM/Open_SLR/wav/ >> logs/evaluation/diffwave.log 2>&1
